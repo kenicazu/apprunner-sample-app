0742804e9378c294ae82b4520ff79142bd96e4a0
kenicazu <kenicazu@amazon.co.jp>
2023-04-13 10:22:16 +0900

    cdk-app-runner.tsの微修正

--- infra/bin/cdk-app-runner.ts
+++ infra/bin/cdk-app-runner.ts
@@ -4,10 +4,9 @@ import * as cdk from 'aws-cdk-lib';
 import { AppRunnerStack } from '../lib/cdk-app-runner-stack';
 import { BaseStack } from "../lib/cdk-base-stack";
 
-const base = new cdk.App();
-const baseStack = new BaseStack(base, "BaseStack");
-
 const app = new cdk.App();
+const baseStack = new BaseStack(app, "BaseStack");
+
 const apprunnerStack = new AppRunnerStack(app, 'AppRunnerStack', {
   vpc: baseStack.vpc,
   containerRepository: baseStack.containerRepository,
